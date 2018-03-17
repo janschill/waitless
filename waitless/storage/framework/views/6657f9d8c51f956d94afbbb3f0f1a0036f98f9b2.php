617761e67987a38271ae56ebe6475c02c428c157
Jan Schill <janschill@me.com>
2018-03-17 14:54:13 +0100

    [TASK] Reset

---
+++ waitless/storage/framework/views/6657f9d8c51f956d94afbbb3f0f1a0036f98f9b2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <h1>Welcome Jan & Stephan</h1>
+    </body>
+</html>
