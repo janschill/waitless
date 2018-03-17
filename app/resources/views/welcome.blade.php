815d812af9dfe814963d8b066dfce992387ca9d9
Jan Schill <janschill@me.com>
2018-03-17 15:23:41 +0100

    Revert to old commit

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -1,19 +1,22 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Waitless</title>
-        </head>
-    <body>
-<h1>Waitless</h1>
-<h2>Wartende Nummern:</h2>
-<ul>
-@foreach ($waitid as $wait)
-<li> {{$wait->number}}</li>
-@endforeach
-</ul>
-<p>(Aus MySQL-Datenbank)</p>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waitless</title>
+</head>
+
+<body>
+    <h1>Waitless</h1>
+    <h2>Wartende Nummern:</h2>
+    <ul>
+        @foreach ($waitid as $wait)
+        <li> {{$wait->number}}</li>
+        @endforeach
+    </ul>
+    <p>(Aus MySQL-Datenbank)</p>
+</body>
+
 </html>
