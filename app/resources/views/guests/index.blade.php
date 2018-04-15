cd6b2be7ed0c2b8183bba8ddbbeea210cc60ac2d
Jan Schill <janschill@me.com>
2018-04-15 18:48:18 +0200

    [TASK] Use seprate layout file to structure page

--- app/resources/views/guests/index.blade.php
+++ app/resources/views/guests/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="de">
+@extends ('layouts.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gäste</title>
-</head>
-
-<body>
+@section('content')
     <h1>Gäste</h1>
     <ul>
         @foreach ($guests as $guest)
@@ -17,6 +9,4 @@
         </li>
         @endforeach
     </ul>
-</body>
-
-</html>
+@endsection
---
+++ app/resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waitless</title>
+</head>
+
+<body>
+
+  @include ('layouts.navigation')
+
+  @yield('content')
+
+  @include ('layouts.footer')
+
+</body>
+
+</html>
