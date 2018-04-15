c90af49314aa9e1a183b130f33a9d4ba55f3c90a
Jan Schill <janschill@me.com>
2018-04-15 12:09:59 +0200

    [CLEANUP] Move files

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(WaitIdTableSeeder::class);
+    }
+}
+
+class WaitIdTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('waitid')->delete();
+
+        WaitId::create(['created_at' => date('Y-m-d H:i:s')]);
+        WaitId::create(['updated_at' => date('Y-m-d H:i:s')]);
+        WaitId::create(['number' => rand(33, 6666)]);
     }
 }
