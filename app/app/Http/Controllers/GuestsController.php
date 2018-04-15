6f6c998f2a02f9666249efcb29cc628a11a6734b
Jan Schill <janschill@me.com>
2018-04-15 22:32:09 +0200

    [WIP][TASK] Implement post method to add new guests

--- app/app/Http/Controllers/GuestsController.php
+++ app/app/Http/Controllers/GuestsController.php
@@ -26,9 +26,13 @@ class GuestsController extends Controller
     public function store()
     {
         Guest::create([
+            'waitid_id' => request('waitid_id'),
+            'state_id' => request('state_id'),
             'group_size' => request('group_size'),
             'comment' => request('comment'),
             'preordered' => request('preordered'),
+            'arrival_time' => now(),
+            'last_state_change' => now(),
         ]);
 
         return redirect('/guests');
