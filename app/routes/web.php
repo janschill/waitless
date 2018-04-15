dcd18618f38fb1ed415736a831af39547859b762
Jan Schill <janschill@me.com>
2018-04-15 18:24:37 +0200

    [TASK] Add guest, waitid, state migration and model

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,18 +1,51 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Guest;
 
 Route::get('/', function () {
-$waitid = DB::table('waitid')->get();
-return view('welcome', compact('waitid'));
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/front', function () {
+    return view('front');
+});
+
+Route::get('/back', function () {
+    return view('back');
+});
+
+
+Route::get('/guests', function () {
+    $guests = Guest::all();
+
+    return view('guests.index', compact('guests'));
+});
+
+Route::get('/guests/{guest}', function ($id) {
+    $guest = Guest::find($id);
+    return view('guests.show', compact('guest'));
+});
+
+
+Route::get('/guests/waiting', function () {
+    $guests = Guest::waiting()->get();
+    return view('guests.index', compact('guests'));
+});
+
+Route::get('/guests/assigned', function () {
+    $guests = Guest::assigned()->get();
+    dd($guests);
+
+    return view('guests.index', compact('guests'));
+});
+
+Route::get('/guests/gone', function () {
+    $guests = Guest::gone()->get();
+
+    return view('guests.index', compact('guests'));
 });
 
