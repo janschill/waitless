c31c732cdbcc3c7c8150ee48ead73d19978ddf92
Jan Schill <janschill@me.com>
2018-04-16 22:12:52 +0200

    [TASK] Add date and date formatting to frontend

--- app/app/Guest.php
+++ app/app/Guest.php
@@ -8,6 +8,8 @@ class Guest extends Model
 {
     protected $fillable = ['waitid_id', 'state_id', 'group_size', 'comment', 'preordered', 'arrival_time', 'last_state_change'];
 
+    protected $dates = ['arrival_time', 'last_state_change'];
+
     public function scopeWaiting($query)
     {
         return $query->where('state_id', 0);
