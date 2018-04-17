8d28fdf44f8647dea433724fd887063106d5e483
Jan Schill <janschill@me.com>
2018-04-17 11:32:49 +0200

    [TASK] Route testing

---
+++ app/tests/Unit/GuestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use \App\Guest;
+
+class GuestTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
