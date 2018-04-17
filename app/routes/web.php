30c146130aa4204f9bba6abf80905d814f94d290
Jan Schill <janschill@me.com>
2018-04-17 11:32:00 +0200

    [CLEANUP] Remove comments

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,21 +11,3 @@ Route::get('/guests/create', 'GuestsController@create');
 Route::get('/guests/{guest}', 'GuestsController@show');
 
 Route::post('/guests', 'GuestsController@store');
-
-// Route::get('/guests/waiting', function () {
-//     $guests = Guest::waiting()->get();
-//     return view('guests.index', compact('guests'));
-// });
-
-// Route::get('/guests/assigned', function () {
-//     $guests = Guest::assigned()->get();
-//     dd($guests);
-
-//     return view('guests.index', compact('guests'));
-// });
-
-// Route::get('/guests/gone', function () {
-//     $guests = Guest::gone()->get();
-
-//     return view('guests.index', compact('guests'));
-// });
