871d5673ca7252183c1b581688c25058213d0925
Jan Schill <janschill@me.com>
2018-04-19 17:25:19 +0200

    [CLEANUP] Remove not needed code

--- app/app/Http/Controllers/GuestsController.php
+++ app/app/Http/Controllers/GuestsController.php
@@ -68,28 +68,4 @@ class GuestsController extends Controller
     {
 
     }
-
-    private function unoccupiedWaitids()
-    {
-        $waitingWaitids = DB::table('waitids')
-            ->join('guests', 'waitids.id', '=', 'guests.waitid_id')
-            ->join('states', 'states.id', '=', 'guests.state_id')
-            ->select('waitids.number')
-            ->where('state', '=', 'wartend')
-            ->get()
-            ->all();
-
-        $waitingWaitidsIndexed = [];
-
-        foreach ($waitingWaitids as $waitingWaitid) {
-            array_push($waitingWaitidsIndexed, $waitingWaitid->number);
-        }
-
-        $avaiblabeWaitidsID = DB::table('waitids')
-            ->select('waitids.*')
-            ->whereNotIn('number', $waitingWaitidsIndexed)
-            ->value('id');
-
-        return $avaiblabeWaitidsID;
-    }
 }
