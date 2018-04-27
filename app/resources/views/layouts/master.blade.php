fb815b2031b0f56c0aafac07692fe1388a7ebd0e
Jan Schill <janschill@me.com>
2018-04-27 23:57:48 +0200

    [TASK] Add PWA iOS meta tags

--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -2,6 +2,11 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
+    <!-- apple pwa meta tags -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <!-- page -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,6 +22,8 @@
 
   @include ('layouts.footer')
 
+
+  <script src="/javascripts/app.min.js"></script>
 </body>
 
 </html>
