1dd7caea3caf138fd9ba22f4d86e17becd0c1150
Jan Schill <janschill@me.com>
2018-05-16 20:15:02 +0200

    [CLEANUP] Refactoring

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -1,5 +1,4 @@
-@extends ('layouts.master_guests')
-@section('content')
+@extends ('layouts.master_guests') @section('content')
 
 
 <table class "list">
@@ -11,32 +10,23 @@
                 <th>Wartedauer</th>
                 <th>Status</th>
         </tr>
-    @foreach ($guests as $guest)
-    <tr>
-            <th>#{{ $guest->waitid->number }}</th>
-            <th>{{ $guest->group_size }}</th>
-            <th><input type="checkbox" id="preordered-cb" onclick="myFunction({{$loop->index}})" {{ $guest->preordered ? 'checked' : ''}}></th>>
-            <th>{{ $guest->comment }}</th>
-            <th>{{ $guest->arrival_time->diffForHumans() }}</th>
-            <th>
-                    <select class="guest__select" name="inputState">
-                            @foreach ($states as $state)
-                            <option class="guest__option" value="state[{{ $state->id }}]" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
-                            @endforeach
+        @foreach ($guests as $guest)
+        <tr>
+                <th>#{{ $guest->waitid->number }}</th>
+                <th>{{ $guest->group_size }}</th>
+                <th>
+                        <input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}>
+                </th>
+                <th>{{ $guest->comment }}</th>
+                <th>{{ $guest->arrival_time->diffForHumans() }}</th>
+                <th>
+                        <select class="guest__select" name="inputState">
+                                @foreach ($states as $state)
+                                <option class="guest__option" value="state[{{ $state->id }}]" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
+                                @endforeach
                         </select>
-            </th>
-    @endforeach
+                </th>
+                @endforeach
 </table>
-
-
-<script>
-function myFunction(id) {
-        <?php use App\Http\Controllers\GuestsController;
-(GuestsController::switchPreorder(1));?>
-alert(id);
-}
-</script>
-
-
 
 @endsection
