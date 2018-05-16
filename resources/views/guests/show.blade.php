e77b98558d4a7eab33638821a8dbc825fc0eb555
Jan Schill <janschill@me.com>
2018-05-16 20:22:22 +0200

    [CLEANUP] Refactoring

--- resources/views/guests/show.blade.php
+++ resources/views/guests/show.blade.php
@@ -1,4 +1,5 @@
-@extends ('layouts.master') @section('content')
+@extends ('layouts.master')
+@section('content')
 <h1>Wartenummer: {{ $guest->waitid_id }}</h1>
 <p>Status: {{ $guest->state_id }}</p>
 <p>Gruppengröße: {{ $guest->group_size }}</p>
