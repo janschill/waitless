05a1ac4959a802fd283d292333a8d052c4927e23
Jan Schill <janschill@me.com>
2018-05-16 20:19:57 +0200

    [CLEANUP] Use 4 spaces not 8

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -2,31 +2,31 @@
 
 
 <table class "list">
-        <tr>
-                <th>Nummer</th>
-                <th>Personen</th>
-                <th>Vorbestellung?</th>
-                <th>Kommentar</th>
-                <th>Wartedauer</th>
-                <th>Status</th>
-        </tr>
-        @foreach ($guests as $guest)
-        <tr>
-                <th>#{{ $guest->waitid->number }}</th>
-                <th>{{ $guest->group_size }}</th>
-                <th>
-                        <input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}>
-                </th>
-                <th>{{ $guest->comment }}</th>
-                <th>{{ $guest->arrival_time->diffForHumans() }}</th>
-                <th>
-                        <select class="guest__select" name="inputState">
-                                @foreach ($states as $state)
-                                <option class="guest__option" value="state[{{ $state->id }}]" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
-                                @endforeach
-                        </select>
-                </th>
+    <tr>
+        <th>Nummer</th>
+        <th>Personen</th>
+        <th>Vorbestellung?</th>
+        <th>Kommentar</th>
+        <th>Wartedauer</th>
+        <th>Status</th>
+    </tr>
+    @foreach ($guests as $guest)
+    <tr>
+        <th>#{{ $guest->waitid->number }}</th>
+        <th>{{ $guest->group_size }}</th>
+        <th>
+            <input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}>
+        </th>
+        <th>{{ $guest->comment }}</th>
+        <th>{{ $guest->arrival_time->diffForHumans() }}</th>
+        <th>
+            <select class="guest__select" name="inputState">
+                @foreach ($states as $state)
+                <option class="guest__option" value="state[{{ $state->id }}]" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
                 @endforeach
+            </select>
+        </th>
+        @endforeach
 </table>
 
 @endsection
