d6eb28c9ea742f529bf59429f743f6ac3c8f1fc6
Jack Dwia <jack.dwia@posteo.net>
2018-06-11 15:45:53 +0200

    [TASK] styled popup, added overlay

--- resources/assets/javascripts/NewGuest.js
+++ resources/assets/javascripts/NewGuest.js
@@ -7,7 +7,7 @@
     }
   };
 
-  var initNewGuest = function ($popup) {
+  var initNewGuest = function ($popup, $overlay) {
     var $formNewOpen = $popup.querySelector('.form__open-new'),
       $form = $popup.querySelector('.form');
 
@@ -17,15 +17,17 @@
       toggleClass($popup, 'popup--expanded');
       toggleClass($form, 'form--expanded');
       toggleClass($formNewOpen, 'form__open-new--expanded');
+      toggleClass($overlay, 'overlay--visible');
     }
   };
 
   document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM new guest');
     var $popup = document.querySelector('.popup');
+    var $overlay = document.querySelector('.overlay');
 
     if ($popup) {
-      initNewGuest($popup);
+      initNewGuest($popup, $overlay);
     }
   });
 }());
