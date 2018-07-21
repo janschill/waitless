0d086df89073ed783e598cf8fa4babe17d769281
Jan Schill <janschill@me.com>
2018-07-21 23:11:43 +0200

    [WIP][TASK] Introduce display table design for list views

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -2,71 +2,64 @@
 
 @section('content')
 
-<div class "list">
-    <ul class="">
-        <li>Nummer</li>
-        <li>Personen</li>
-        <li>Vorbestellung?</li>
-        <li>Kommentar</li>
-        <li>Wartedauer</li>
-        <li>Status</li>
-        <li>
+<div class="table table--current">
+    <div class="table__head">
+        <div class="table__row">
+            <div class="table__column">Nummer</div>
+            <div class="table__column">Personen</div>
+            <div class="table__column">Bestellt</div>
+            <div class="table__column">Hinweis</div>
+            <div class="table__column">Wartedauer</div>
+            <div class="table__column">Status</div>
+            <div class="table__column">Bearbeiten</div>
+        </div>
+    </div>
+    <div class="table__body table__body--active">
+        @foreach ($guests as $guest)
+            @if ($guest->state_id == 1)
+                <div class="table__row">
+                    <input class="" type="hidden" name="guestId" value="{{ $guest->id }}">
+                    <div class="table__column">#{{ $guest->waitid->number }}</div>
+                    <div class="table__column">{{ $guest->group_size }}</div>
+                    <div class="table__column"><input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}></div>
+                    <div class="table__column">{{ $guest->comment }}</div>
+                    <div class="table__column">{{ $guest->arrival_time->diffForHumans() }}</div>
+                    <div class="table__column">
+                        @foreach ($states as $state)
+                            <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $guest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
+                        @endforeach
+                    </div>
+                    <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+                </div>
+            @endif
+        @endforeach
+    </div>
+    <div class="table__body table__body--history">
+        @foreach ($guests as $guest)
+            @if ($guest->state_id != 1)
+                <div class="table__row">
+                    <input class="" type="hidden" name="guestId" value="{{ $guest->id }}">
+                    <div class="table__column">#{{ $guest->waitid->number }}</div>
+                    <div class="table__column">{{ $guest->group_size }}</div>
+                    <div class="table__column"><input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}></div>
+                    <div class="table__column">{{ $guest->comment }}</div>
+                    <div class="table__column">{{ $guest->arrival_time->diffForHumans() }}</div>
+                    <div class="table__column">
+                        @foreach ($states as $state)
+                            <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $guest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
+                        @endforeach
+                    </div>
+                    <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+                </div>
+            @endif
+        @endforeach
+    </div>
+</div>
+
             <div class="popup">
                 <a href="#" class="form__open-new">+</a>
                 <a href="#" class="form__close-new form__close-new--hidden">x</a>
 
                 @include ('guests.new-guest-form')
             </div>
-        </li>
-    </ul>
-    <ul class="guest__list guest__list--waiting">
-        @foreach ($guests as $guest)
-            @if ($guest->state_id == 1  )
-                <li class="guest">
-                    <input class="guest__id" type="hidden" name="guestId" value="{{ $guest->id }}">
-                    <span>#{{ $guest->waitid->number }}</span>
-                    <span>{{ $guest->group_size }}</span>
-                    <span>
-                        <input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}>
-                    </span>
-                    <span>{{ $guest->comment }}</span>
-                    <span>{{ $guest->arrival_time->diffForHumans() }}</span>
-                    <span>
-                        <select class="guest__select guest__select--state" name="inputState">
-                            @foreach ($states as $state)
-                            <option class="guest__option" value="{{ $state->id }}" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
-                            @endforeach
-                        </select>
-                    </span>
-                </li>
-            @endif
-        @endforeach
-    </ul>
-    <br>
-    <h3>History</h3>
-    <ul class="guest__list guest__list--placed">
-        @foreach ($guests as $guest)
-            @if ($guest->state_id != 1  )
-                <li class="guest">
-                    <input class="guest__id" type="hidden" name="guestId" value="{{ $guest->id }}">
-                    <span>#{{ $guest->waitid->number }}</span>
-                    <span>{{ $guest->group_size }}</span>
-                    <span>
-                        <input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}>
-                    </span>
-                    <span>{{ $guest->comment }}</span>
-                    <span>{{ $guest->arrival_time->diffForHumans() }}</span>
-                    <span>
-                        <select class="guest__select guest__select--state" name="inputState">
-                            @foreach ($states as $state)
-                            <option class="guest__option" value="{{ $state->id }}" {{ $state->state === $guest->state->state ? 'selected=\'selected\'' : '' }}>{{ $state->state }}</option>
-                            @endforeach
-                        </select>
-                    </span>
-                </li>
-            @endif
-        @endforeach
-    </ul>
-</div>
-
 @endsection
--- resources/views/waitids/index.blade.php
+++ resources/views/waitids/index.blade.php
@@ -1,28 +1,32 @@
 @extends ('layouts.master') @section('content')
 
-<table class "list">
-    <tr>
-        <th>Wartenummer</th>
-        <th>Erstellt am</th>
-        <th>Zuletzt vergeben an</th>
-        <th>Vergabeanzahl</th>
-        <th></th>
-    </tr>
-    @foreach ($waitids as $waitid)
-    <tr>
-        <th>#{{ $waitid->number }}</th>
-        <th>{{ $waitid->created_at->toFormattedDateString() }}</th>
-        <th>Implement me!</th>
-        <th>{{ $waitid->guests->count() }}</th>
-        <th>
-            <form action="/waitids/{{ $waitid->id }}" method="post">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit">X</button>
-            </form>
-        </th>
-    </tr>
-    @endforeach
-</table>
+<div class="table">
+    <div class="table__head">
+        <div class="table__row">
+            <div class="table__column">Wartenummer</div>
+            <div class="table__column">Erstellt am</div>
+            <div class="table__column">Zuletzt vergeben an</div>
+            <div class="table__column">Vergabeanzahl</div>
+            <div class="table__column">Entfernen</div>
+        </div>
+    </div>
+    <div class="table__body">
+        @foreach ($waitids as $waitid)
+        <div class="table__row">
+            <div class="table__column">#{{ $waitid->number }}</div>
+            <div class="table__column">{{ $waitid->created_at->toFormattedDateString() }}</div>
+            <div class="table__column">Implement me!</div>
+            <div class="table__column">{{ $waitid->guests->count() }}</div>
+            <div class="table__column">
+                <form action="/waitids/{{ $waitid->id }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button class="button button--delete" type="submit"></button>
+                </form>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
