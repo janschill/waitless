ac3fa0d1e487b7d74d8f0ef10429dd01b827d16b
Jan Schill <janschill@me.com>
2018-07-21 19:58:04 +0200

    [CLEANUP] Use foreach instead of for

--- resources/assets/javascripts/ChangeAnchors.js
+++ resources/assets/javascripts/ChangeAnchors.js
@@ -1,16 +1,15 @@
 (function () {
-
   var initAnchorChanger = function () {
     var a = document.getElementsByTagName('a');
 
-    for (var i = 0; i < a.length; i++) {
-      if (!a[i].onclick && a[i].getAttribute('target') !== '_blank') {
-        a[i].onclick = function () {
+    a.forEach(element => {
+      if (!element.onclick && element.getAttribute('target') !== '_blank') {
+        element.onclick = function () {
           window.location = this.getAttribute('href');
           return false;
         }
       }
-    }
+    });
   };
 
   document.addEventListener('DOMContentLoaded', function () {
