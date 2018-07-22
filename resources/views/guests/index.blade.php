e49bb357f8fb814fda1015c8b384540366adc4d7
Jan Schill <janschill@me.com>
2018-07-22 22:33:23 +0200

    [CLEANUP] Remove if state checking state

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -16,42 +16,38 @@
     </div>
     <div class="table__body table__body--active">
         @foreach ($guests as $iterator=>$guest)
-            @if ($guest->state_id == 1)
-                <div class="table__row{{ $iterator % 2 === 0 ? ' table__row--highlight' : '' }}">
-                    <input class="" type="hidden" name="guestId" value="{{ $guest->id }}">
-                    <div class="table__column">#{{ $guest->waitid->number }}</div>
-                    <div class="table__column">{{ $guest->group_size }}</div>
-                    <div class="table__column"><input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}></div>
-                    <div class="table__column">{{ $guest->comment }}</div>
-                    <div class="table__column">{{ $guest->arrival_time->diffForHumans() }}</div>
-                    <div class="table__column">
-                        @foreach ($states as $state)
-                            <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $guest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
-                        @endforeach
-                    </div>
-                    <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+            <div class="table__row{{ $iterator % 2 === 0 ? ' table__row--highlight' : '' }}">
+                <input class="" type="hidden" name="guestId" value="{{ $guest->id }}">
+                <div class="table__column">#{{ $guest->waitid->number }}</div>
+                <div class="table__column">{{ $guest->group_size }}</div>
+                <div class="table__column"><input class="input-preorder" type="checkbox" {{ $guest->preordered ? 'checked' : ''}}></div>
+                <div class="table__column">{{ $guest->comment }}</div>
+                <div class="table__column">{{ $guest->arrival_time->diffForHumans() }}</div>
+                <div class="table__column">
+                    @foreach ($states as $state)
+                        <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $guest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
+                    @endforeach
                 </div>
-            @endif
+                <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+            </div>
         @endforeach
     </div>
     <div class="table__body table__body--history">
         @foreach ($historyGuests as $iterator=>$historyGuest)
-            @if ($historyGuest->state_id != 1)
-                <div class="table__row{{ $iterator % 2 !== 0 ? ' table__row--highlight' : '' }}">
-                    <input class="" type="hidden" name="guestId" value="{{ $historyGuest->id }}">
-                    <div class="table__column">#{{ $historyGuest->waitid->number }}</div>
-                    <div class="table__column">{{ $historyGuest->group_size }}</div>
-                    <div class="table__column"><input class="input-preorder" type="checkbox" {{ $historyGuest->preordered ? 'checked' : ''}}></div>
-                    <div class="table__column">{{ $historyGuest->comment }}</div>
-                    <div class="table__column">{{ $historyGuest->arrival_time->diffForHumans() }}</div>
-                    <div class="table__column">
-                        @foreach ($states as $state)
-                            <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $historyGuest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
-                        @endforeach
-                    </div>
-                    <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+            <div class="table__row{{ $iterator % 2 !== 0 ? ' table__row--highlight' : '' }}">
+                <input class="" type="hidden" name="guestId" value="{{ $historyGuest->id }}">
+                <div class="table__column">#{{ $historyGuest->waitid->number }}</div>
+                <div class="table__column">{{ $historyGuest->group_size }}</div>
+                <div class="table__column"><input class="input-preorder" type="checkbox" {{ $historyGuest->preordered ? 'checked' : ''}}></div>
+                <div class="table__column">{{ $historyGuest->comment }}</div>
+                <div class="table__column">{{ $historyGuest->arrival_time->diffForHumans() }}</div>
+                <div class="table__column">
+                    @foreach ($states as $state)
+                        <a data-state-id="{{ $state->id }}" class="button{{ $state->state === $historyGuest->state->state ? ' button--active' : '' }}" href="/">{{ $state->state }}</a>
+                    @endforeach
                 </div>
-            @endif
+                <div class="table__column"><a class="button" href="/">Bearbeiten</a></div>
+            </div>
         @endforeach
     </div>
 </div>
