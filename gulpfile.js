c402430593cbf563dd871093097bf80f5bc04df5
Jan Schill <janschill@me.com>
2018-08-01 13:12:06 +0200

    [TASK] Update some dependencies and use webpack to load laravel-echo and pusher for now

--- gulpfile.js
+++ gulpfile.js
@@ -5,67 +5,84 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   rename = require('gulp-rename'),
   stylelint = require('gulp-stylelint'),
-  autoprefixer = require('gulp-autoprefixer');
+  autoprefixer = require('gulp-autoprefixer'),
+  babel = require('gulp-babel');
 
 // Function that catch errors and will therefore prevent gulp to exit the watch task as long as an on listener is set.
 function swallowError(error) {
   console.log(error.messageFormatted);
-}
+};
 
-gulp.task('watch', function () {
-  gulp.watch('resources/assets/stylesheets/**/*.scss', ['stylelint', 'css']);
-  gulp.watch('resources/assets/javascripts/**/*.js', ['eslint', 'uglify']);
+gulp.task('lintJavaScripts', function () {
+  gulp.src([
+    'resources/assets/javascripts/**/*.js'
+  ])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError())
+  .on('error', swallowError);
 });
 
-gulp.task('eslint', function () {
-  return gulp.src(['resources/assets/javascripts/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+gulp.task('processJavaScripts', function () {
+  gulp.src([
+    'node_modules/jquery/dist/jquery.js',
+    'resources/assets/javascripts/custom/**/*.js'
+  ])
+  .pipe(babel({
+    presets: ['env']
+  }))
+  .pipe(concat('app.min.js'))
+  .pipe(uglify({
+    mangle: true,
+    compress: true
+  }))
+  .on('error', swallowError)
+  .pipe(gulp.dest('public/javascripts/'));
 });
 
-gulp.task('uglify', function () {
-  gulp
-    .src([
-      'node_modules/pusher-js/dist/web/pusher.min.js',
-      'node_modules/laravel-echo/dist/echo.js',
-      'node_modules/jquery/dist/jquery.js',
-      'resources/assets/javascripts/**/*.js'
-    ])
-    .pipe(concat('app.min.js'))
-    .pipe(uglify({
-      mangle: true,
-      compress: true
-    }))
-    .on('error', swallowError)
-    .pipe(gulp.dest('public/javascripts/'));
+gulp.task('lintStyleSheets', function () {
+  gulp.src([
+    'resources/assets/stylesheets/**/*.scss'
+  ])
+  .pipe(stylelint({
+    reporters: [{
+      formatter: 'string',
+      console: true
+    }]
+  }))
+  .on('error', swallowError);
 });
 
-gulp.task('stylelint', function () {
-  return gulp
-    .src('resources/assets/stylesheets/**/*.scss')
-    .pipe(stylelint({
-      reporters: [{
-        formatter: 'string',
-        console: true
-      }]
-    }));
-});
-
-gulp.task('css', function () {
-  gulp
-    .src('resources/assets/stylesheets/app.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .on('error', swallowError)
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions', '> 5%']
-    }))
-    .pipe(rename('app.min.css'))
-    .pipe(gulp.dest('public/stylesheets/'));
+gulp.task('processStyleSheets', function () {
+  gulp.src([
+    'resources/assets/stylesheets/app.scss'
+  ])
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .on('error', swallowError)
+  .pipe(autoprefixer({
+    browsers: ['last 4 versions', '> 5%']
+  }))
+  .pipe(rename('app.min.css'))
+  .pipe(gulp.dest('public/stylesheets/'));
 });
 
 gulp.task('default', [
-  'eslint', 'uglify', 'stylelint', 'css'
+  'lintJavaScripts',
+  'processJavaScripts',
+  'lintStyleSheets',
+  'processStyleSheets'
 ]);
+
+gulp.task('watch', function () {
+  gulp.watch('resources/assets/stylesheets/**/*.scss', [
+    'lintStyleSheets',
+    'processStyleSheets'
+  ]);
+  gulp.watch('resources/assets/javascripts/**/*.js', [
+    'lintJavaScripts',
+    'processJavaScripts'
+  ]);
+});
+
