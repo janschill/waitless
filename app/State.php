d65785c1bad873aec27310e5505765f35dd79dbc
Jan Schill <janschill@me.com>
2018-08-07 16:17:36 +0200

    [WIP][TASK] Introduce new state and start implementing it to frontend

--- app/State.php
+++ app/State.php
@@ -3,11 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class State extends Model
 {
     public function guests()
     {
         return $this->hasMany(Guest::class);
+    }
+
+    public static function getDefiniteStates()
+    {
+        $states = DB::table('states')
+            ->where('id', '!=', 4)
+            ->get();
+        return $states;
     }
 }
--- resources/views/guests/pending.blade.php
+++ resources/views/guests/pending.blade.php
@@ -1,6 +1,14 @@
 <div class="table__caption">Zugewiesen <span class="table__caption-sub">Gäste</span></div>
 <ul class="pending__list">
-  <li class="box">Bla</li>
-  <li class="box">Bla</li>
-  <li class="box">Bla</li>
+  @foreach ($assignedGuests as $iterator=>$assignedGuest)
+    <li class="box">
+      <h2 class="box__headline box__headline--large">T.10</h2>
+      <h3 class="box__headline">#{{ $assignedGuest->waitid->number }}</h3>
+      <p>Gruppe: {{ $assignedGuest->group_size }}</p>
+      <div class="box__time">
+        <p>A.{{ $assignedGuest->arrival_time->format('H:i') }}</p>
+        <p>Z.{{ $assignedGuest->last_state_change->format('H:i') }}</p>
+      </div>
+    </li>
+  @endforeach
 </ul>
