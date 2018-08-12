dfb463104d878bba8045a8037e3d665bb958e907
Jan Schill <janschill@me.com>
2018-08-12 15:02:55 +0200

    [WIP][TASK] Work my way through the bugs, that are present when creating new guests in frontend

--- resources/assets/javascripts/custom/listenToGuestChannel.js
+++ resources/assets/javascripts/custom/listenToGuestChannel.js
@@ -4,14 +4,14 @@
      * Fired when new guest is created
      */
     window.Echo.channel('guests').listen('GuestCreated', event => {
-      GuestRow.createGuestRow(Position.end(), 'active', event.guest, event.unoccupiedWaitids, event.waitidNumber, event.states);
+      GuestRow.createGuestRow(Position.end(), 'active', event.guest, event.unoccupiedWaitids, event.waitidNumber, event.statesForCurrent, event.statesForHistory);
     });
 
     /**
      * Fired when existing guest is update
      */
     window.Echo.channel('guests').listen('GuestUpdated', event => {
-      GuestRow.updateGuestRow(event.guest, event.unoccupiedWaitids, event.waitidNumber, event.states);
+      GuestRow.updateGuestRow(event.guest, event.unoccupiedWaitids, event.waitidNumber, event.statesForCurrent, event.statesForHistory);
     });
   }
 
--- resources/views/guests/pending.blade.php
+++ resources/views/guests/pending.blade.php
@@ -1,7 +1,7 @@
 <div class="table__caption">Zugewiesen <span class="table__caption-sub">Gäste</span></div>
 <ul class="box__list box__list--pending">
   @foreach ($assignedGuests as $iterator=>$assignedGuest)
-    <li data-guest-id="{{ $assignedGuest->id }}" class="box box--guest">
+    <li data-guest-id="{{ $assignedGuest->id }}" id="guest-id-{{ $assignedGuest->id }}" class="box box--guest">
       {{-- <h2 class="box__headline box__headline--large">T.10</h2> --}}
       <h3 class="box__headline box__headline--large">#{{ $assignedGuest->waitid->number }}</h3>
       <p>Personen: {{ $assignedGuest->group_size }}</p>
@@ -12,7 +12,12 @@
       <form class="form form--set-state" action="/guests/{{ $assignedGuest->id }}" method="POST">
         {{ csrf_field() }}
         {{ method_field('PATCH') }}
-        <input type="hidden" name="guest_state_id" value="{{ $stateSeated->id }}">
+        <input class="input__guest-id"  type="hidden" name="guest_id" value="{{ $assignedGuest->id }}">
+        <input class="input__guest-waitid-id" type="hidden" name="guest_waitid_id" value="{{ $assignedGuest->waitid->id }}">
+        <input class="input__guest-group-size" type="hidden" name="guest_group_size" value="{{ $assignedGuest->group_size }}">
+        <input class="input__guest-preordered" type="hidden" name="guest_preordered" value="{{ $assignedGuest->preordered }}">
+        <input class="input__guest-comment" type="hidden" name="guest_comment" value="{{ $assignedGuest->comment }}">
+        <input class="input__guest-state-id" type="hidden" name="guest_state_id" value="{{ $assignedGuest->state_id }}">
       </form>
     </li>
   @endforeach
