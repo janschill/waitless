a577b8b75aaa191db713d29e1a91d7e00b406686
Jan Schill <janschill@me.com>
2018-08-18 21:34:20 +0200

    [TASK] Add statistics logic for day/month/year evaluation

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -1,4 +1,9 @@
 @extends ('layouts.master')
 @section('content')
-<canvas data-content-url="/statistics/guests/2018/06" class="chart"></canvas>
+<ul class="statistics-control">
+  <li data-url="day" class="button-toggle button-toggle--long">Tag</li>
+  <li data-url="month" class="button-toggle button-toggle--long">Monat</li>
+  <li data-url="year" class="button-toggle button-toggle--long">Jahr</li>
+</ul>
+<canvas data-content-url="/statistics/guests/2018" class="chart"></canvas>
 @endsection
