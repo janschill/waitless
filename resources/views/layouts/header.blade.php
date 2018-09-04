ccaf65a34cb682245a1abdd68d7da452544e2724
Jan Schill <janschill@me.com>
2018-09-04 16:20:20 +0200

    [CLEANUP] Rework header section for better looks – closes #50

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,12 +2,16 @@
   <div class="header__branding">
   @include ('layouts.logo')
   </div>
+  <ul class="header__statistics-list">
+    <li class="header__statistics-list-item">100 Gäste</li>
+    <li class="header__statistics-list-item">2 Personen</li>
+    <li class="header__statistics-list-item">10 Minuten</li>
+  </ul>
   <nav class="navigation">
     <ul class="navigation__list">
       <li class="navigation__list-item"><a class="navigation__link" href="/guests">Gäste</a></li>
       <li class="navigation__list-item"><a class="navigation__link" href="/waitids">Wartenummer</a></li>
       <li class="navigation__list-item"><a class="navigation__link" href="/statistics">Statistik</a></li>
-      <li class="navigation__list-item"><a class="navigation__link" href="/">Einstellungen</a></li>
     </ul>
   </nav>
   <div class="hamburger">
