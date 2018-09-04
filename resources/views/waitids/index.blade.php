4887c2bd5a4f99fa7d2e67f845638013b7af2785
Jan Schill <janschill@me.com>
2018-09-04 07:02:50 +0200

    [CLEANUP] Move add new guest button to row – closes #46

--- resources/views/waitids/index.blade.php
+++ resources/views/waitids/index.blade.php
@@ -39,7 +39,7 @@
 </ul>
 <div class="background background--create-new background--hidden"></div>
 <div data-form-id="form-new-waitid" class="popup popup--new popup--new-waitid">
-    <div class="popup__toggle popup__toggle--new-waitid"></div>
+    <div class="popup__toggle-waitid popup__toggle--new-waitid"></div>
     <div class="popup__content">
         @include ('waitids.create')
     </div>
