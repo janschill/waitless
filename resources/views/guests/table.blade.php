e050b48e604fd1c212e5891ec0de52fc4aa5545d
Jan Schill <janschill@me.com>
2018-09-05 08:53:31 +0200

    [CLEANUP] Use display grid instead of table

--- resources/views/guests/table.blade.php
+++ resources/views/guests/table.blade.php
@@ -1,14 +1,12 @@
 <div class="background background--update background--hidden"></div>
 <div class="table table--current">
-    <div class="table__head table__head--hidden">
-        <div class="table__row">
-            <div class="table__column">Nummer</div>
-            <div class="table__column">Personen</div>
-            <div class="table__column">o. R.</div>
-            <div class="table__column">Hinweis</div>
-            <div class="table__column">Wartezeit</div>
-            <div class="table__column">Status</div>
-        </div>
+    <div class="table__head">
+        <div class="table__column">Nummer</div>
+        <div class="table__column">Personen</div>
+        <div class="table__column">o. R.</div>
+        <div class="table__column">Hinweis</div>
+        <div class="table__column">Wartezeit</div>
+        <div class="table__column">Status</div>
     </div>
     <div class="table__caption table__caption--table">Aktuell <span class="table__caption-sub">wartende Gäste ({{ count($guests) }})</span></div>
     <div class="table__body table__body--active">
@@ -27,16 +25,6 @@
 </div>
 <div class="table table--history">
     <div class="table__caption table__caption--table">Historie <span class="table__caption-sub">letzten 10 Gäste</span></div>
-    <div class="table__head table__head--hidden">
-        <div class="table__row">
-            <div class="table__column">Nummer</div>
-            <div class="table__column">Personen</div>
-            <div class="table__column">o. R.</div>
-            <div class="table__column">Hinweis</div>
-            <div class="table__column">Wartezeit</div>
-            <div class="table__column">Status</div>
-        </div>
-    </div>
     <div class="table__body table__body--history">
         @foreach ($historyGuests as $iterator=>$historyGuest)
             @include('guests.table-row', ['guest' => $historyGuest, 'states' => $statesForHistory])
