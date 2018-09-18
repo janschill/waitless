c970de6500c4b22602484b7b166590fc3be7b6fc
Jan Schill <janschill@me.com>
2018-09-18 14:01:02 +0200

    [TASK] Add nohup to server start

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ composer install --optimize-autoloader
 echo -e "\n\033[33mInstalling node packages …\033[0m"
 npm install
 gulp
+npm run dev
 echo -e "\n\033[33mConfigurating application …\033[0m"
 php artisan key:generate
 php artisan migrate
@@ -27,11 +28,12 @@ sed -i '' 's/APP_ENV=local/APP_ENV=production/g' .env
 
 echo -e "Installation finished. Thank you."
 
-read -p "Do you want me to start a server on localhost? [y/N]: " -n 1 -r
+read -p "Do you want me to start a server on localhost:8080? [y/N]: " -n 1 -r
 echo
 if ! [[ $REPLY =~ ^[Yy]$ ]]; then
   echo -e "\n\033[33mStarting server on http://localhost:8080/ …\033[0m"
-  php artisan serve --port=8080
+  echo -e "\n\033[33mClose Terminal tab or kill process with following ID to shutdown server:\033[0m"
+  nohup php artisan serve --port=8080 > localhost.log 2>&1 &
 fi
 
 # read -p "Do you want me to take out the trash (remove node_modules etc.)? [y/N]: " -n 1 -r
