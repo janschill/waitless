0d2595560f887b368341efa4ec79bb8830bd9ef1
Jan Schill <janschill@me.com>
2018-09-19 08:25:18 +0200

    [CLEANUP] Notification adapt to title block

--- resources/views/layouts/notification.blade.php
+++ resources/views/layouts/notification.blade.php
@@ -1,7 +1,7 @@
 <aside class="notification">
-  <header class="notification__header"><h1 class="title">Neuer Gast</h1></header>
+  <header class="notification__header"><h2></h2></header>
   <main class="notification__main">
-    <h2 class="title"><strong>#234</strong></h2>
-    <p class="notification__paragraph">2 Personen – Brauchen Ecktisch</p>
+    <h2><strong></strong></h2>
+    <p class="notification__paragraph"></p>
   </main>
 </aside>
